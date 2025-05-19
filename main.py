a81de0507bc46e35a8c3db4b79466f0a47299520
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-20 04:34:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from log_manager import LogManager
-from data_base import DatabaseWrapper
+from data_base import DatabaseWrapper, DatabaseInitializer
+from file_work import File
 
 def main():
     log_manager = LogManager("config/logging_config.json")
     log_manager.setup_logging()
 
-    db = DatabaseWrapper("config")
-    db.connect()
+    database_wrapper = DatabaseInitializer().initialize("config/database")
+    database_wrapper.connect()
 
 if __name__ == "__main__":
     main()
