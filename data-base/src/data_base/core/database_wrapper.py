f075431e335dfe5409642292eca149f9e312f42b
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-20 04:39:24 +0700

    Update database_wrapper.py

--- data-base/src/data_base/core/database_wrapper.py
+++ data-base/src/data_base/core/database_wrapper.py
@@ -1,39 +1,64 @@
-from .._config.config_loader import JSONConfigLoader
-from .._services.mysql.mysql_connection import MySQLConnection as Connection
+from .._services.mysql.mysql_connection import MySQLConnection
 from .._services.mysql.mysql_table_repository import MySQLTableRepository
-from .._utils.messages import Messages as Msg
+from .._shared.messages.msg_error import ConnectionsError, RepositoriesError
+from .._shared.messages.msg_debug import RepositoriesDebug
+from .._shared.messages.msg_info import RepositoriesInfo
 
 class DatabaseWrapper:
-    def __init__(self, config_path: str):
-        self._config_loader = JSONConfigLoader(config_path)
-        self._connection = None
-        self._tables_config = None
+    def __init__(self, database_config: dict, tables_config: dict, queries_dict: dict):
+        self._database_config = database_config
+        self._tables_config = tables_config
+        self._queries_dict = queries_dict
+
         self._repositories = {}
+        self._connection = None
 
-    def connect(self):
-        db_config = self._config_loader.load("database_config").get("database", {})
-        self._connection = Connection(db_config)
-        if not self._connection.connect():
-            return False
+    def connect(self) -> None:
+        self._connection = MySQLConnection(self._database_config)
+        if self._connection.connect():
+            self._initialize_repositories()
 
-        self._tables_config = self._config_loader.load("tables_config").get("tables", {})
-        for table_name in self._tables_config:
-            queries = self._config_loader.load(f"queries/{table_name}")
-            self._repositories[table_name] = MySQLTableRepository(
-                self._connection.raw, table_name, queries
-            )
-            Msg.Complete.table_initialized(table_name)
-        return True
+    def _initialize_repositories(self) -> None:
+        if not self._connection or not self._connection.is_connected():
+            ConnectionsError.connection_miss(self._database_config.get("name"))
+            return
+
+        missing_queries = [table for table in self._tables_config if table not in self._queries_dict]
+        if missing_queries: RepositoriesError.missing_queries(missing_queries)
+
+        initialized_tables = []
+        for table_name, queries in self._queries_dict.items():
+            try:
+                self._repositories[table_name] = MySQLTableRepository(
+                    self._connection.raw, table_name, queries
+                )
+                initialized_tables.append(table_name)
+                RepositoriesDebug.table_initialized(table_name)
+            except Exception as e:
+                RepositoriesError.table_initialized(table_name, str(e))
+        RepositoriesInfo.tables_initialized(initialized_tables)
+
+    def table(self, name: str):
+        if not self._connection or not self._connection.is_connected():
+            ConnectionsError.connection_miss(self._database_config.get("name"))
+            return
+
+        return self._repositories.get(name)
 
     def reset(self):
+        if not self._connection or not self._connection.is_connected():
+            ConnectionsError.connection_miss(self._database_config.get("name"))
+            return
+
         for repo in reversed(self._repositories.values()):
             repo.drop()
         for repo in self._repositories.values():
             repo.create()
 
     def close(self):
+        if not self._connection or not self._connection.is_connected():
+            ConnectionsError.connection_miss(self._database_config.get("name"))
+            return
+
         if self._connection:
             self._connection.close()
-
-    def table(self, name: str):
-        return self._repositories.get(name)
