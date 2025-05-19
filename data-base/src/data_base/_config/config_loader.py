cdd2ee9eaa9dfc5b4a4b7a41d22d98987ae37ac9
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-20 04:35:40 +0700

    Delete config_loader.py

--- data-base/src/data_base/_config/config_loader.py
+++
@@ -1,8 +0,0 @@
-from file_work import File
-
-class JSONConfigLoader:
-    def __init__(self, folder: str):
-        self.folder = folder
-
-    def load(self, file_name: str, extension: str = "json") -> dict:
-        return File.read(file_path=f"{self.folder}/{file_name}.{extension}")
