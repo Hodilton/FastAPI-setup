f7be99ca61f4a535cbb3b12a45445fed7cb73b8c
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-20 04:36:17 +0700

    Update __init__.py

--- data-base/src/data_base/__init__.py
+++ data-base/src/data_base/__init__.py
@@ -1,3 +1,4 @@
 from .core.database_wrapper import DatabaseWrapper
+from .core.database_initializer import DatabaseInitializer
 
-__all__ = ["DatabaseWrapper"]
+__all__ = ["DatabaseWrapper", "DatabaseInitializer"]
