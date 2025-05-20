66b5abf177c64330dfb925bfb9c6b963ae7ee0c8
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-21 02:35:43 +0700

    Update msg_debug.py

--- data-base/src/data_base/_shared/messages/msg_debug.py
+++ data-base/src/data_base/_shared/messages/msg_debug.py
@@ -5,19 +5,6 @@ class ConnectionDebug(MsgBase):
     def connection_already_established(cls, database_name: str) -> None:
         cls._logger.debug(f"Connection to database '{database_name}' already established")
 
-class ConfigLoadDebug(MsgBase):
-    @classmethod
-    def database_load_config(cls) -> None:
-        cls._logger.debug(f"Database config loaded successfully")
-
-    @classmethod
-    def tables_load_config(cls) -> None:
-        cls._logger.debug(f"Tables config loaded successfully")
-
-    @classmethod
-    def table_load_queries(cls, table_name: str) -> None:
-        cls._logger.debug(f"Queries for table '{table_name}' loaded successfully")
-
 class RepositoriesDebug(MsgBase):
     @classmethod
     def table_initialized(cls, table_name: str) -> None:
