07f53160a786d3494dc8c5b73a7ace6dae62b028
Hodilton <120922249+Hodilton@users.noreply.github.com>
2025-05-26 15:51:46 +0700

    r

--- src/app/config.py
+++ src/app/config.py
@@ -1,11 +1,13 @@
 # src/app/config.py
 from pydantic_settings import BaseSettings
 from typing import List
+from pathlib import Path
 
 class Settings(BaseSettings):
     FIRST_SUPERUSER: str
     FIRST_SUPERUSER_PASSWORD: str
     DATABASE_URL: str
+
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = "supersecretkey123"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 11520
@@ -13,6 +15,6 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "My FastAPI App"
 
     class Config:
-        env_file = "./.env"
+        env_file = str(Path(__file__).resolve().parents[1] / ".env")
 
 settings = Settings()
